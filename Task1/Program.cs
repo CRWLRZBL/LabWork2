a4a0a832c643e03bb8b059ca3823d9462893425a
Crowler 1337 <gamevax25@gmail.com>
2024-06-24 15:45:16 +0300

    Initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 97, 45, 32, 65, 83, 23, 15 };
+Console.WriteLine("Введите значение искомого элемента");
+
+int position = -1;
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == number)
+    {
+        position = i + 1;
+        break;
+    }
+}
+Console.WriteLine($"Позиция = {position}");
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = { 97, 45, 32, 65, 83, 23, 15 };
+Console.WriteLine("Введите нужный элемент");
+int right = 0, left = 0, number = 0;
+int middle = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == middle)
+    { 
+        number = i;
+        break;
+    }
+    else if (array[i] < middle)
+    {
+        left = middle - 1;
+        number = -1;
+        break;
+    }
+    else right = middle + 1;
+}
+Console.WriteLine(number);
